60206225806aefe71d1173d9bbe99d381f0dd9b9
hocus-p0cus <isitresilient@gmail.com>
2025-12-06 21:45:41 +0300

    extracted UI control button icons

--- src/components/ZoomControls.jsx
+++ src/components/ZoomControls.jsx
@@ -1,30 +1,13 @@
 import React from 'react';
 import { ZoomIn, ZoomOut, Maximize2 } from './icons';
+import { IconButton } from './ui/IconButton';
 
 export const ZoomControls = ({ onZoomIn, onZoomOut, onReset }) => {
   return (
-    <div className="absolute top-4 right-4 flex flex-col gap-2">
-      <button
-        onClick={onZoomIn}
-        className="p-2 bg-slate-800/80 backdrop-blur rounded hover:bg-slate-700"
-        title="Zoom In"
-      >
-        <ZoomIn size={20} />
-      </button>
-      <button
-        onClick={onZoomOut}
-        className="p-2 bg-slate-800/80 backdrop-blur rounded hover:bg-slate-700"
-        title="Zoom Out"
-      >
-        <ZoomOut size={20} />
-      </button>
-      <button
-        onClick={onReset}
-        className="p-2 bg-slate-800/80 backdrop-blur rounded hover:bg-slate-700"
-        title="Reset View"
-      >
-        <Maximize2 size={20} />
-      </button>
+    <div className="absolute top-4 right-4 lg:top-6 lg:right-6 flex flex-col gap-2">
+      <IconButton icon={ZoomIn} onClick={onZoomIn} title="Zoom In" />
+      <IconButton icon={ZoomOut} onClick={onZoomOut} title="Zoom Out" />
+      <IconButton icon={Maximize2} onClick={onReset} title="Reset View" />
     </div>
   );
 };
